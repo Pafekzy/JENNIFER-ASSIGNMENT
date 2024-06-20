79d8b54df03d80c0afc84194ba3698607d3a5f92
Pafekzy <pafekzy@gmail.com>
2024-06-20 13:28:15 +0100

    first line

---
+++ AllType.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AllType {
+	public static void main(String[] args) {
+
+	float roomTemperature = 35.0f;
+	short number = 500;
+	long alphabet = 5000;
+	double height = 17.9;
+	byte binary = 101;
+	int leight = 90;
+	char cheap = '1';
+	boolean isItTrue = true;
+	System.out.printf("%f %d %d %d %d %d %c %b",roomTemperature,number,alphabet,height,binary,leight,cheap,isItTrue);
+
+           
+} 
+} 
